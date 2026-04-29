e1c3568bb651fa72ba79a2d95334801a9baa8807
Andrew Pampuch <apampuch@protonmail.com>
2026-04-29 20:14:10 +0900

    Adds more slash commands to bot.

--- savageroller.py
+++ savageroller.py
@@ -3,7 +3,6 @@ import discord
 import os # default module
 from die_roller import *
 from dotenv import load_dotenv
-from enum import Enum
 
 load_dotenv() # load all the variables from the env file
 bot = discord.Bot()
@@ -24,58 +23,127 @@ async def roll(ctx: discord.ApplicationContext, msg: str):
     try:
         roll_data = parse_tokens(msg)
         roll_results = roll_savage_dice(roll_data)
-        await ctx.respond(package_roll(msg, roll_data, **roll_results))
+        await ctx.respond(package_roll(msg, roll_data, **roll_results))  # type: ignore
     except ValueError as e:
         await ctx.respond(e)
     except Exception:
-        await ctx.respond("Some other error happened, see the log for details.")
+        await ctx.respond("An error occurred, see the log for details.")
         raise
-    
+
 # INITIATIVE
-
 @bot.slash_command(name="fight", description="Start a new fight")
-async def fight(ctx: discord.ApplicationContext, characters: str):
-    await ctx.respond("NYI")
+async def fight(ctx: discord.ApplicationContext, char_names: str):
+    chars_list = list(set(map(lambda x: x.strip(), char_names.split(','))))
 
-@bot.slash_command(name="deal", description="Deal in new characters")
-async def deal(ctx: discord.ApplicationContext, characters: str):
-    await ctx.respond("NYI")
+    try:
+        message = characters.fight(chars_list, ctx.guild_id, ctx.channel_id)
+        await ctx.respond(message)
+    except Exception:
+        await ctx.respond("An error occurred, see the log for details.")
+        raise
+
+@bot.slash_command(name="deal_in", description="Deal in new characters")
+async def deal_in(ctx: discord.ApplicationContext, char_names: str):
+    chars_list = list(set(map(lambda x: x.strip(), char_names.split(','))))
+
+    try:
+        message = characters.add_to_initiative(chars_list, ctx.guild_id, ctx.channel_id)
+        await ctx.respond(message)
+    except Exception:
+        await ctx.respond("An error occurred, see the log for details.")
+        raise
 
 @bot.slash_command(name="remove", description="Remove a character from the current fight")
-async def remove(ctx: discord.ApplicationContext, characters: str):
-    await ctx.respond("NYI")
+async def remove(ctx: discord.ApplicationContext, char_names: str):
+    chars_list = list(set(map(lambda x: x.strip(), char_names.split(','))))
 
-@bot.slash_command(name="card", description="Give characters a card")
-async def card(ctx: discord.ApplicationContext, characters: str):
-    await ctx.respond("NYI")
+    try:
+        message = characters.remove_from_initiative(chars_list, ctx.guild_id, ctx.channel_id)
+        await ctx.respond(message)
+    except Exception:
+        await ctx.respond("An error occurred, see the log for details.")
+        raise
+
+@bot.slash_command(name="give_card", description="Give a characters a new card")
+async def give_card(ctx: discord.ApplicationContext, char_name: str):
+    try:
+        message = characters.deal_new_card_to_character(char_name, ctx.guild_id, ctx.channel_id)
+        await ctx.respond(message)
+    except Exception:
+        await ctx.respond("An error occurred, see the log for details.")
+        raise
 
 @bot.slash_command(name="list", description="Lists everyone in the current fight")
-async def card(ctx: discord.ApplicationContext):
-    await ctx.respond("NYI")
+async def list_fight(ctx: discord.ApplicationContext):
+    try:
+        message = characters.show_list(ctx.guild_id, ctx.channel_id)
+        await ctx.respond(message)
+    except Exception:
+        await ctx.respond("An error occurred, see the log for details.")
+        raise
 
-@bot.slash_command(name="give_tactician_card", description="Gives a chraracter in the current fight a tactician card")
-async def give_tactician_card(ctx: discord.ApplicationContext, card_number: int, character: str):
-    await ctx.respond("NYI")
+
+@bot.slash_command(name="assign_tactician_card", description="Assigns a character a tactician card from another character's tactician card lst.")
+async def assign_tactician_card(ctx: discord.ApplicationContext, tactician_character: str, card: str, recipient_character: str):
+    try:
+        message = characters.assign_tactician_card(tactician_character, card, recipient_character, ctx.guild_id, ctx.channel_id)
+        await ctx.respond(message)
+    except Exception:
+        await ctx.respond("An error occurred, see the log for details.")
+        raise
+
+@bot.slash_command(name="choose_card", description="Swaps a given character's action card with another one from among their unused cards.")
+async def choose_card(ctx: discord.ApplicationContext, char_name: str, card: str):
+    try:
+        message = characters.choose_card(char_name, card, ctx.guild_id, ctx.channel_id)
+        await ctx.respond(message)
+    except ValueError as e:
+        await ctx.respond(str(e))
+    except Exception:
+        await ctx.respond("An error occurred, see the log for details.")
+        raise
+
+@bot.slash_command(name="quick_redraw", description="Makes a character redraw cards until their current card is greater than 5. This is mostly intended to be used with char_names who have both Level Headed and Quick, and works on characters without Quick.")
+async def quick_redraw(ctx: discord.ApplicationContext, char_name: str):
+    try:
+        message = characters.quick_redraw(char_name, ctx.guild_id, ctx.channel_id)
+        await ctx.respond(message)
+    except Exception:
+        await ctx.respond("An error occurred, see the log for details.")
+        raise
 
 # CHARACTERS AND BENNIES
 @bot.slash_command(name="new_character", description="Create a new character")
-async def new_character(ctx: discord.ApplicationContext, character_name: str):
+async def new_character(ctx: discord.ApplicationContext, char_name: str):
     try:
-        characters.add_character(character_name, ctx.guild_id, False)
-        await ctx.respond(f"Created character {character_name}.")
+        message = characters.add_character(char_name, ctx.guild_id, False)
+        await ctx.respond(message)
+    except ValueError as e:
+        await ctx.respond(str(e))
     except Exception:
         await ctx.respond("An error occurred. Character was not created.")
 
 @bot.slash_command(name="delete_character", description="Delete a character")
 async def delete_character(ctx: discord.ApplicationContext, character_name: str):
     try:
-        characters.delete_character(character_name, ctx.guild_id)
-        await ctx.respond(f"Deleted character {character_name}.")
+        message = characters.remove_character(character_name, ctx.guild_id)
+        await ctx.respond(message)
     except LookupError:
         await ctx.respond(f"Character {character_name} does not exist.")
 
+@bot.slash_command(name="rename_character", description="Rename a character")
+async def rename_character(ctx: discord.ApplicationContext, old_name: str, new_name: str):
+    try:
+        message = characters.rename_character(old_name, new_name, ctx.guild_id)
+        await ctx.respond(message)
+    except ValueError as e:
+        await ctx.respond(str(e))
+    except Exception:
+        await ctx.respond("An error occurred. Character was not created.")
+
 @bot.slash_command(name="add_edges", description="Give a character edges, comma separated")
 async def add_edges(ctx: discord.ApplicationContext, character_name: str, edges: str):
+    # convert comma separated list to set
     edges_list = set(map(lambda x: x.strip(), edges.split(',')))
 
     try:
@@ -89,7 +157,7 @@ async def add_edges(ctx: discord.ApplicationContext, character_name: str, edges:
 
 @bot.slash_command(name="remove_edges", description="Remove an edge from a character")
 async def remove_edges(ctx: discord.ApplicationContext, character_name: str, edges: str):
-    edges_list = set(map(lambda x: x.strip(), edges.split(',')))
+    edges_list = list(map(lambda x: x.strip(), edges.split(',')))
     
     try:
         deleted_rowcount = characters.remove_edges_from_character(character_name, ctx.guild_id, edges_list)
