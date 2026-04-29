98625327a052110f8b93a98d942a40067972373d
Andrew Pampuch <apampuch@protonmail.com>
2026-04-29 18:48:17 +0900

    Adds way to convert from symbol representation to character representation and vice versa of card suits.

--- decks.py
+++ decks.py
@@ -6,3 +6,28 @@ for rank in ("A", "K", "Q", "J", "10", "9", "8", "7", "6", "5", "4", "3", "2"):
         DECK_OF_CARDS.append(suit + rank)
 
 PlayingCardDeck = tuple(DECK_OF_CARDS)
+
+suit_dict = {
+    "S": "♠",
+    "H": "♥",
+    "D": "♦",
+    "C": "♣"
+}
+
+suit_dict_inverted = {v: k for k, v in suit_dict.items()}
+
+"""
+Changes from pure character format (SA, C2) to symbol format (♠A, ♣2)
+"""
+def char_to_symbol(card: str) -> str:
+    suit: str = card[0]
+    suit = suit_dict[suit]
+
+    return suit + card[1:]
+
+
+def symbol_to_Char(card: str) -> str:
+    suit: str = card[0]
+    suit = suit_dict_inverted[suit]
+
+    return suit + card[1:]
